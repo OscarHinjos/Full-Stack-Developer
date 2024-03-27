1f3fd718388e35e19c4b8f1e49b1ba525a6796a2
Óscar Hinjos <oscarhinjoslopez@gmail.com>
2024-03-27 09:24:57 +0100

    Proyect 2 - BlackJack
    Optimizations and security code.

--- JavaScript/Proyect 2 - BlackJack/assets/js/black-jack.js
+++ JavaScript/Proyect 2 - BlackJack/assets/js/black-jack.js
@@ -5,146 +5,120 @@
 - 2S = Two of Spades (Spadas)
 */
 
-const typesCards = ['C', 'D', 'H', 'S'];
-const especialCards = ['A', 'J', 'Q', 'K'];
+// Funcion anonimas autoinvocadas
+// Mejora de seguridad
+(() => {
+    'use strict';
 
-let deck = [];
-let pointsPlayer = 0;
-let pointsCrupier = 0;
+    const typesCards = ['C', 'D', 'H', 'S'];
+    const especialCards = ['A', 'J', 'Q', 'K'];
 
-// HTML References
-const btnNuevo = document.querySelector('#btnNuevo');
-const btnPedir = document.querySelector('#btnPedir');
-const btnDetener = document.querySelector('#btnDetener');
-const spanList = document.querySelectorAll('span')
-const playerCards = document.querySelector('.carts-player')
-const crupierCards = document.querySelector('.cards-crupier')
+    let deck = [];
+    let pointsPlayer = 0;
+    let pointsCrupier = 0;
 
+    const btnNuevo = document.querySelector('#btnNuevo');
+    const btnPedir = document.querySelector('#btnPedir');
+    const btnDetener = document.querySelector('#btnDetener');
+    const spanList = document.querySelectorAll('span');
+    const playerCards = document.querySelector('.carts-player');
+    const crupierCards = document.querySelector('.cards-crupier');
 
-
-
-// Crea una nueva baraja
-const createDeck = () => {
-    for (let i = 2; i <= 10; i++) {
+    const createDeck = () => {
+        for (let i = 2; i <= 10; i++) {
+            for (let type of typesCards) {
+                deck.push(i + type);
+            }
+        }
         for (let type of typesCards) {
-            deck.push(i + type);
+            for (let especialCard of especialCards) {
+                deck.push(especialCard + type);
+            }
         }
-    }
-    for (let type of typesCards) {
-        for (let especialCard of especialCards) {
-            deck.push(especialCard + type);
+        return shuffle(deck);
+    };
+
+    const shuffle = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
         }
-    }
+        return array;
+    };
 
-    deck = _.shuffle(deck); // Randomizamos la baraja
+    const askCard = () => {
+        if (deck.length === 0) {
+            throw new Error("No hay cartas en el deck");
+        }
+        return deck.pop();
+    };
 
-    return deck;
-};
+    const evaluateCard = (card) => {
+        const value = card.substring(0, card.length - 1);
+        return (!isNaN(value)) ? parseInt(value) : (value === 'A') ? 11 : 10;
+    };
 
-deck = createDeck();
-
-// Esta función permite tomar una carta
-const askCard = (deck) => {
-    if (deck.length === 0) {
-        throw "No hay cartas en el deck";
-    }
-    const card = deck.pop();
-
-    return card;
-};
-
-
-const evalueCard = (card) => {
-    const value = card.substring(0, card.length - 1);
-    return (!isNaN(value)) ? parseInt(value) :
-        (value === 'A') ? 11 : 10;
-};
-
-
-const turnCrupier = (pointsPlayer) => {
-    do {
-        const card = askCard(deck);
-        pointsCrupier += evalueCard(card);
-        spanList[1].innerText = pointsCrupier;
+    const showCard = (card, points, container) => {
+        points += evaluateCard(card);
+        spanList[container].innerText = points;
         const imgCard = document.createElement('img');
         imgCard.src = `assets/cards/${card}.png`;
         imgCard.classList = "card";
-        crupierCards.append(imgCard);
-        if (pointsPlayer > 21) {
-            break;
-        }
+        container === 0 ? playerCards.append(imgCard) : crupierCards.append(imgCard);
+        return points;
+    };
 
-    } while ((pointsCrupier < pointsPlayer) && (pointsCrupier <= 21));
+    const endGame = () => {
+        setTimeout(() => {
+            let message;
+            if (pointsCrupier === pointsPlayer) {
+                message = "Empate";
+            } else if (pointsPlayer > 21 || (pointsCrupier <= 21 && pointsCrupier > pointsPlayer)) {
+                message = "Gana el Crupier";
+            } else {
+                message = "Gana el Jugador";
+            }
+            alert(message);
+        }, 20);
+    };
 
+    const init = () => {
+        deck = createDeck();
+        btnPedir.addEventListener('click', () => {
+            pointsPlayer = showCard(askCard(), pointsPlayer, 0);
+            if (pointsPlayer > 21 || pointsPlayer === 21) {
+                btnPedir.disabled = true;
+                btnDetener.disabled = true;
+                endGame();
+            }
+        });
 
-    setTimeout(() => {
-        if(pointsCrupier === pointsPlayer){
-            alert("Empate")
-        }else if(pointsPlayer > 21){
-            alert("Gana el Crupier")
-        }else if(pointsCrupier > 21){
-            alert("Gana el jugador")
-        }else{
-            alert("Gana el Crupier")
-        }
-    }, 20);
+        btnDetener.addEventListener('click', () => {
+            btnDetener.disabled = true;
+            btnPedir.disabled = true;
+            while (pointsCrupier < pointsPlayer && pointsCrupier <= 21) {
+                pointsCrupier = showCard(askCard(), pointsCrupier, 1);
+            }
+            endGame();
+        });
 
+        btnNuevo.addEventListener('click', () => {
+            deck = [];
+            deck = createDeck();
+            btnPedir.disabled = false;
+            btnDetener.disabled = false;
+            spanList.forEach(span => span.innerText = 0);
+            playerCards.innerHTML = '';
+            crupierCards.innerHTML = '';
+            pointsCrupier = 0;
+            pointsPlayer = 0;
+        });
+    };
 
-}
-
-
-// Events 
-
-// Player
-btnPedir.addEventListener('click', () => {
-    const card = askCard(deck);
-    pointsPlayer += evalueCard(card);
-    spanList[0].innerText = pointsPlayer;
-    const imgCard = document.createElement('img');
-    imgCard.src = `assets/cards/${card}.png`;
-    imgCard.classList = "card";
-    playerCards.append(imgCard);
-
-    if (pointsPlayer > 21) {
-        console.warn("Lo siento mucho, perdiste");
-        btnPedir.disabled = true;
-        btnDetener.disabled = true
-        turnCrupier(pointsPlayer)
-    } else if (pointsPlayer === 21) {
-        console.warn("21, Genial!");
-        btnPedir.disabled = true;
-        btnDetener.disabled = true
-        turnCrupier(pointsPlayer)
-    }
-});
-
-// Crupier
-
-btnDetener.addEventListener('click', () => {
-    btnDetener.disabled = true
-    btnPedir.disabled = true
-    turnCrupier(pointsPlayer)
-
-});
+    init(); // Iniciar el juego cuando se cargue la página por primera vez
+})();
 
 
-btnNuevo.addEventListener('click', () => {
-    // Resetear todo
-    deck = []
-    deck = createDeck();
-    btnPedir.disabled = false;
-    btnDetener.disabled = false
-    for(let i = 0 ; i < spanList.length ; i++){
-        spanList[i].innerText = 0
-    }
-
-    playerCards.innerHTML = '';
-    crupierCards.innerHTML = '';
-    pointsCrupier = 0
-    pointsPlayer = 0
-
-
-});
 
 
 
